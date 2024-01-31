9dcd5494ba93d404a125d97b96dd94a5313b1bb9
Juma <93912358+Nashon-Juma@users.noreply.github.com>
2024-01-31 11:48:55 +0300

    majin

--- inputDic.py
+++ inputDic.py
@@ -1,13 +1,13 @@
-majina={}
+majinaa={}
 Active=True
 
-while majina:
+while majinaa:
     name=input('\n who are u ? ')
     answer=input("what's yr favorite animal ? ")
     repeat=input('would u like to continue')
-    majina[name]=answer
+    majinaa[name]=answer
     if repeat=='no':
         Active=False
 print('-----------POLLS RESULTS---------------')
-for name,answer in majina.items():
+for name,answer in majinaa.items():
     print(name+ 'would like to have'+ answer)

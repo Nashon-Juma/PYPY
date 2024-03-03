e33a54d26c4fd0522565f08565bde81f3e4a84d7
juma <nashonhhh@gmail.com>
2024-03-03 17:40:47 +0300

    Joke

--- from.py
+++ from.py
@@ -1,5 +1,5 @@
 def count_words(filename):
-        """Count the approximate number of words in a file."""
+        ""Count the approximate number of words in a file.""
         try:
             with open(filename, encoding='utf-8') as f:
                 contents = f.read()
